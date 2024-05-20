81ea432deb3110f48901d722cd178896c43361e4
EITAN GABRIEL TOPOROVSKAJA <47295991@ort>
2024-05-20 15:20:07 -0300

    navbar agregada

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,39 @@
 import './App.css';
 import AcercaDeMi from './acercaDeMi';
-import Inicio from './inicio';
-import Certificaciones from './certificaciones';
 import Conocimientos from './conocimientos';
 import Contacto from './contacto';
 import Educacion from './educacion';
 import Experiencia from './experiencia';
+<header>
 
+<div class="collapse navbar-collapse" id="navbarSupportedContent">
+  <ul class="navbar-nav ms-auto me-2">
+    <li class="nav-item">
+      <a class="nav-link" href="#AcercaDeMi">Acerca de mi</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#Conocimientos">Conocimientos</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#Contacto">Contacto</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#Educacion">Educacion</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#Experiencia">Experiencia</a>
+    </li>
+  </ul>
+</div>
+
+</header>
 
 function App() {
   const nombre="Eitan"
   const apellido="Toporovskaja"
   return (
     <div className="App">
-     <AcercaDeMi nombre={nombre} apellido={apellido} />
-      <Inicio nombre={nombre} apellido={apellido} /> 
-      <Certificaciones/>
+      <AcercaDeMi nombre={nombre} apellido={apellido} />
       <Conocimientos/>
       <Contacto/>
       <Educacion/>
--- src/experiencia/index.jsx
+++ src/experiencia/index.jsx
@@ -1,12 +1,25 @@
+import imagenCertificacion from "../images/certificadoCurso"
+import imagenHackerRank from "../images/certificadoIntermedioHackerRank.png"
 function Experiencia () {
     return(
         <div class ="row">
             <div class= "col-md-6">
                 <div class = "card mb-3 aos-init aos-animate">
                     <div class="card-body px-3 py-2">
-                         <p>
-                            nulo
-                        </p>
+                        <div class="grid-item">
+                            <a href="https://www.cloudskillsboost.google/public_profiles/9b84c1cb-b07a-424b-8ea1-c018485a0d27/badges/5044189?utm_medium=social&utm_source=linkedin&utm_campaign=ql-social-share">
+                            <figure class="portfolio-item">
+                            <img src={imagenCertificacion} alt="" />
+                            </figure>
+                            </a>        
+                        </div>
+                        <div class="grid-item">
+                            <a href="https://www.hackerrank.com/certificates/10bd5af04dfd">
+                                <figure class="portfolio-item">
+                                <img src={imagenHackerRank} alt="" />    
+                                </figure>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
