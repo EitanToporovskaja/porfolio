5f0654543ab2a246cb5e4cb8e82e34ec3a2b0dbb
EITAN GABRIEL TOPOROVSKAJA <47295991@ort>
2024-05-20 15:59:22 -0300

    navbar agregada, falta css

--- src/educacion/index.jsx
+++ src/educacion/index.jsx
@@ -1,14 +1,19 @@
 function Educacion () {
     return(
-        <div class="col-md-6">
-            <div class="card mb-3 aos-init aos-animate">
-                <div class="card-header px-3 py-2">
-                    <div class="d-flex justify-content-between"> 
-                        <div>
-                        <h3 class="h5 mb-1">Bachiller Técnico con orientación en Informática</h3>
-                            <div class="text-muted text-small">
-                            "Escuela ORT Yatay " 
-                            <small>(2020-2024)</small>
+        <div class="container-narrow">
+            <div class="text-center mb-5">
+                <h2 class="marker marker-center">"Contacto"</h2>
+            </div>
+            <div class="col-md-6">
+                <div class="card mb-3 aos-init aos-animate">
+                    <div class="card-header px-3 py-2">
+                        <div class="d-flex justify-content-between"> 
+                            <div>
+                            <h3 class="h5 mb-1">Bachiller Técnico con orientación en Informática</h3>
+                                <div class="text-muted text-small">
+                                "Escuela ORT Yatay " 
+                                <small>(2020-2024)</small>
+                                </div>
                             </div>
                         </div>
                     </div>
---
+++ src/navbar/index.jsx
@@ -0,0 +1,27 @@
+function Navbar() {
+
+    return(
+        <header>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ms-auto me-2">
+            <li class="nav-item">
+              <a class="nav-link" href="#AcercaDeMi">Acerca de mi</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#Conocimientos">Conocimientos</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#Contacto">Contacto</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#Educacion">Educacion</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#Experiencia">Experiencia</a>
+            </li>
+          </ul>
+        </div>
+      </header>
+    )
+}
+export default Navbar
