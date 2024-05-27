6c98a5c7df243f8a4ff47b1e6e4e79794d7bc848
EITAN GABRIEL TOPOROVSKAJA <47295991@ort>
2024-05-27 15:44:22 -0300

    se acomodaron las imgs experiencia

--- src/experiencia/index.jsx
+++ src/experiencia/index.jsx
@@ -10,25 +10,17 @@ function Experiencia () {
                 <div class="text-center mb-5">
                     <h2 class="marker marker-center">Experiencia</h2>
                 </div>
-                <div class ="row">
-                    <div class= "col-md-6">
-                        <div>
-                            <div>
-                                <div class="grid-item">
-                                    <a href="https://www.cloudskillsboost.google/public_profiles/9b84c1cb-b07a-424b-8ea1-c018485a0d27/badges/5044189?utm_medium=social&utm_source=linkedin&utm_campaign=ql-social-share">
-                                    <figure class="portfolio-item">
-                                    <img src={imagenCertificacion} alt="" />
-                                    </figure>
-                                    </a>        
-                                </div>
-                                <div class="grid-item">
-                                    <a href="https://www.hackerrank.com/certificates/10bd5af04dfd">
-                                        <figure class="portfolio-item">
-                                        <img src={imagenHackerRank} alt="" />    
-                                        </figure>
-                                    </a>
-                                </div>
-                            </div>
+                <div>
+                    <div>
+                        <div class="imgExp">
+                            <a href="https://www.cloudskillsboost.google/public_profiles/9b84c1cb-b07a-424b-8ea1-c018485a0d27/badges/5044189?utm_medium=social&utm_source=linkedin&utm_campaign=ql-social-share">
+                                <img src={imagenCertificacion} alt="" />
+                            </a>        
+                        </div>
+                        <div class="imgExp">
+                            <a href="https://www.hackerrank.com/certificates/10bd5af04dfd">
+                                <img src={imagenHackerRank} alt="" />    
+                            </a>
                         </div>
                     </div>
                 </div>
